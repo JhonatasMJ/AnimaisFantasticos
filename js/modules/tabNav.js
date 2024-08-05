5512a3fd95d412e5ad9f1cd7e6aa67b2f2727203
JhonatasMJ <jhonatasjhmj@gmail.com>
2024-08-05 15:34:55 -0300

    Refatorando tabNav

--- js/modules/tabNav.js
+++ js/modules/tabNav.js
@@ -1,27 +1,41 @@
-export default function  initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  tabContent[0].classList.add("ativo"); //Ja vai entrar no site com ele ativo
+export default class TabNav {
+  constructor (menu,content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
 
-  //  IF = Verificar se existe elemento em tabContent e tabMenu
-  if (tabMenu.length && tabContent.length) {
-    function activeTab(index) {
-      tabContent.forEach((section) => {
+    this.classeAtiva = 'ativo'
+  
+  }
+//Ativa a tab de acordo com  o index da mesma
+  activeTab(index) {
+      this.tabContent.forEach((section) => {
         //Remove a classe ativo após ela estar ativa em outro lugar, ela ficará ativa somente em uma section por vez
-        section.classList.remove("ativo");
+        section.classList.remove(this.classeAtiva);
       });
-      const direcao = tabContent[index].dataset.anime;
-      tabContent[index].classList.add("ativo",direcao);
+      const direcao = this.tabContent[index].dataset.anime;
+      this.tabContent[index].classList.add(this.classeAtiva,direcao);
     }
 
-    tabMenu.forEach((itemMenu, index) => {
-      //O index é o numero das imagens 0,1,2
-      itemMenu.addEventListener("click", () => {
-        activeTab(index);
+    //Adiciona os eventos nas tabs
+    addTabNavEvent() {
+      this.tabMenu.forEach((itemMenu, index) => {
+
+        //O index é o numero das imagens 0,1,2
+        itemMenu.addEventListener("click", () => {
+          this.activeTab(index);
+        });
       });
-    });
-  }
-}
+    }
+    
+    init ()  {
+      if (this.tabMenu.length && this.tabContent.length) {
+        //Ativar primeiro item
+        this.activeTab(0)
+        this.addTabNavEvent()
+       }
+      }
+    }
+
 
 
 
