66cdb6ac72abcfb9a65854e305e82d8ea113008e
JhonatasMJ <jhonatasjhmj@gmail.com>
2024-08-06 15:45:59 -0300

    Modal Refatorado

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,4 +1,3 @@
-//Export default é pra quando só tem uma coisa que queremos exportar
 export default class ScrollSuave {
   constructor(links, opcoes) { //Criando um constructor pra poder usar sempre
     this.linksInternos = document.querySelectorAll(links); //Pega os links que passei no parametro la no script
@@ -13,22 +12,24 @@ export default class ScrollSuave {
   scrollToSection(event) {
     event.preventDefault(); //Tira o padrão dos links que é levar para a seção
     const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href); //Pegando a seção conforme clico no link, com o codigo de cima
-    section.scrollIntoView(this.opcoes);
+    if (href !== '#') { // Verifica se o href não é apenas '#'
+      const section = document.querySelector(href); //Pegando a seção conforme clico no link, com o codigo de cima
+      if (section) { // Verifica se a seção existe
+        section.scrollIntoView(this.opcoes);
+      }
+    }
   }
 
   addLinkEvent() {
     this.linksInternos.forEach((link) => {
-      link.addEventListener("click",this.scrollToSection);
-  
+      link.addEventListener("click", this.scrollToSection);
     });
   }
 
   init() {
     if(this.linksInternos.length) {
       this.addLinkEvent();
-
     }
-    return this
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+import Modal from "./modules/modal.js";
 import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import TabNav from "./modules/tabNav.js";
-import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdown from "./modules/dropdown.js";
 import initMenu from "./modules/menu.js";
@@ -12,25 +12,22 @@ import Accordion from "./modules/accordion.js";
 
 // Ordem de execução em cascata, se necessário
 
+const scrollSuave = new ScrollSuave('a[href^="#"]'); //Primeira opção são os links da onde vou clicar
+scrollSuave.init()
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
+
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
 
 initDropdown();
 initfuncionamento();
-
-initModal();
 initTooltip();
 initMenu();
 initAnimacaoScroll();
 initFetch();
 initBitCoin();
-
-
-
-const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section')
-tabNav.init()
-
-const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]'); //Primeira opção são os links da onde vou clicar
-scrollSuave.init()
-
-const accordion = new Accordion('[data-anime="accordion"] dt');
-accordion.init();
