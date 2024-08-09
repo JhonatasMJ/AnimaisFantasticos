4edc258bf086c6753580f0ac116f2aecbf666023
JhonatasMJ <jhonatasjhmj@gmail.com>
2024-08-08 21:59:19 -0300

    Dropdown-Refatorado

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,21 +1,43 @@
 import cliqueFora from "./cliqueFora.js";
 
-export default function initDropdown() {
-  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+export default class Dropdown {
+  constructor(dropdownMenus) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
 
-  dropdownMenus.forEach((menu) => {
-    ["touchstart", "click"].forEach((userEvent) => {
-      //Array para passar 2 eventos em um unico addEvent
-      menu.addEventListener(userEvent, handleClick);
-    });
-  });
+    if(eventos === undefined) { //Se não for definido irá seguir o padrão setado
+      this.eventos = ['touchstart', 'click'];
+    } else {
+      this.eventos = eventos
+    }
+    this.classeAtiva = 'ativo'
+    this.ativarDropdown = this.ativarDropdown.bind(this);
 
-  function handleClick(event) {
+  }
+
+  //Ativa o Dropdown e adiciona a função que observa o clique fora dele
+  ativarDropdown(event) {
     event.preventDefault(); //Não abrir o index
-    this.classList.add("ativo");
-    cliqueFora(this, ["touchstart", "click"], () => {
+    const element = event.currentTarget
+    element.classList.add(this.classeAtiva);
+    cliqueFora(element, this.eventos, () => {
       //Se clicar ou fazer o evento de touch para fora, remover a classe ativo
-      this.classList.remove("ativo");
+    element.classList.remove(this.classeAtiva);
     });
+  }
+//Adiciona os eventos ao dropdown menu
+  addDropdownEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      this.eventos.forEach((userEvent) => {
+        //Array para passar 2 eventos em um unico addEvent
+        menu.addEventListener(userEvent, this.ativarDropdown);
+      });
+    });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownEvent();
+      return this;
+    }
   }
 }
