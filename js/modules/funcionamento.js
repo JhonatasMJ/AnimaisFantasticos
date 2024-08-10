2842e07f7eca2144f79475bda23fa25809273a96
JhonatasMJ <jhonatasjhmj@gmail.com>
2024-08-10 08:38:32 -0300

    Refatorando-Funcionamento

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,18 +1,48 @@
-export default function initfuncionamento() {
-  const funcionamento = document.querySelector("[data-semana]");
-  const diasSemana = funcionamento.dataset.semana.split(",").map(Number); //Transformando em array e em string
-  const horaSemana = funcionamento.dataset.horario.split(",").map;
+export default class Funcionamento {
+  constructor (funcionamento,ativo) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.ativo = ativo
 
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horarioAgora = dataAgora.getHours();
-
-  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1; //Verifica se o dia de hoje é um dos dias que está aberto na semana
-
-  const horarioAberto =
-    horarioAgora >= horaSemana[0] && horarioAgora < horaSemana[1]; //Verifica se o horario de abertura e fechamento
-
-  if (semanaAberto && horarioAberto) {
-    funcionamento.classList.add("aberto"); //Se ambos estiverem aberto, adiciona a classe aberto
   }
-}
+
+  dadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(",").map(Number); //Transformando em array e em string
+    this.horaSemana = this.funcionamento.dataset.horario.split(",").map;
+  }
+
+
+  dadosAgora() {
+   this.dataAgora = new Date();
+   this.diaAgora = this.dataAgora.getDay();
+   this.horarioAgora = this.dataAgora.getUTCHours() - 3; //Horario do BRASIL
+
+  }
+
+
+  estaAberto() {
+    const semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1; //Verifica se o dia de hoje é um dos dias que está aberto na semana
+  
+    const horarioAberto =
+      this.horarioAgora >= this.horaSemana[0] && this.horarioAgora < this.horaSemana[1]; //Verifica se o horario de abertura e fechamento
+
+      return  semanaAberto && horarioAberto;
+  }
+
+
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.ativo); //Se ambos estiverem aberto, adiciona a classe aberto
+    }
+  }
+  
+    init () {
+      if(this.funcionamento) {
+        this.dadosFuncionamento()
+        this.dadosAgora()
+        this.ativaAberto();
+  
+      }
+      return this
+    }
+  }  
+
